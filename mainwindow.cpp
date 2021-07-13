eed61d022cc801754ccf0936bc8313fb693c482a
q5sys <jt@obs-sec.com>
2021-07-12 20:28:37 -0400

    some updates

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDialog>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QString>
+#include <QGridLayout>
+#include <QLineEdit>
+#include <QLabel>
+#include <QDialogButtonBox>
+
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
 }
@@ -17,12 +26,11 @@ MainWindow::~MainWindow(){
 
 void MainWindow::on_encodePushButton_clicked(){
   imageFile = QFileDialog::getOpenFileName(this, tr("Open Image"), tr("Image File (*.png *.jpg *.bmp)"));
-  passPopup* passPopup= new passPopup(this);
+  passPopup();
   //save temp text to file as base64
   plainTextEditor *cur = currentEditor();
-    QTextStream b64text;
-    b64text << *cur.toUtf8().toBase64();
-  }
+  QTextStream b64text;
+  b64text << *cur.toUtf8().toBase64();
   //send base64 string with pass into encoding class
   encodeCLASS(imageFile, password, b64text);
 }
@@ -66,4 +74,57 @@ void MainWindow::on_closePushButton_clicked(){
   //if yes - exit
   // shred any files left
   //if no - cancel
+}
+
+//===========================================
+//  trith source code
+//  Copyright (c) 2021, q5sys
+//  Available under the MIT license
+//  See the LICENSE file for full details
+//===========================================
+
+
+void MainWindow::passPopup(QWidget parent) : QDialog(parent) {
+makePopup();
+setWindowTitle( tr("Enter password") );
+setModal( true );
+}
+
+void MainWindow::makePopup(){
+// set up the layout
+QGridLayout formGridLayout = new QGridLayout( this );
+linePassword = new QLineEdit( this );
+linePassword->setEchoMode( QLineEdit::Password );
+
+// create label
+labelPassword = new QLabel( this );
+labelPassword->setText( tr( "Password" ) );
+labelPassword->setBuddy( linePassword" );
+
+// create buttons
+buttons = new QDialogButtonBox( this );
+buttons->addButton( QDialogButtonBox::Ok );
+buttons->addButton( QDialogButtonBox::Cancel );
+buttons->button( QDialogButtonBox::Ok )->setText( tr("Ok") );
+buttons->button( QDialogButtonBox::Cancel )->setText( tr("Cancel") );
+
+// connect slots
+connect( buttons->button( QDialogButtonBox::Cancel ), SIGNAL (clicked()), this, SLOT (close()) );
+connect( buttons->button( QDialogButtonBox::Ok ), SIGNAL (clicked()), this, SLOT (setPassword()) );
+
+// Add UI layout
+QGridLayout formGridLayout = new QGridLayout( this );
+formGridLayout->addWidget( labelUsername, 0, 0 );
+formGridLayout->addWidget( comboUsername, 0, 1 );
+formGridLayout->addWidget( labelPassword, 0, 0 );
+formGridLayout->addWidget( editPassword, 0, 1);
+formGridLayout->addWidget( buttons, 2, 0, 1, 2 );
+linePassword->setFocus();
+}
+
+void MainWindow::setPassword(QString &password){
+linePassword->setText( password );
+password = linePassword->text();
+// close this dialog
+close();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -8,6 +8,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QLabel>
+#include <QFileDialog>
+#include <QDialogButtonBox>
+#include <QGridLayout>
+
 
 QT_BEGIN_NAMESPACE namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -18,6 +24,15 @@ class MainWindow : public QMainWindow {
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    openImageFile();
+    QDialog makePopup();
+    QDialog passPopup();
+    QLineEdit linePassword;
+    QLabel labelPassword;
+    QString password;
+    QFileDialog imageFile;
+    QDialogButtonBox buttons;
+    QString setPassword();
 
 private slots:
     void on_encodePushButton_clicked();
